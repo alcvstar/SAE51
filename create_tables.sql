2650f615464f4b9b67005b1048dde0ad048bb581
alcvstar <alcvstar@gmail.com>
2023-11-24 09:31:59 +0100

    ajout commentaires ficheirs sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,31 +1,33 @@
--- Création de la base de données
+-- création de la BDD
 CREATE DATABASE IF NOT EXISTS sae51_bdd;
 USE sae51_bdd;
 
--- Création de la table users
+-- création de la table users
 CREATE TABLE IF NOT EXISTS users (
-    id_user INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
+    id_user INT AUTO_INCREMENT PRIMARY KEY, -- on définit un "id" (différent pour chaque table) qui fera office de clé primaire
+    nom VARCHAR(255) NOT NULL, -- on crée les colonnes correspondant aux données que l'on veut avoir dans nos tables
+    prenom VARCHAR(255) NOT NULL, -- précision : on utilise l'argument NOT NULL pour que les colonnes ne puissent pas être vides
     poste VARCHAR(255) NOT NULL
 );
 
--- Création de la table maintenance avec la clé étrangère
+-- création de la table maintenance reliée à la table users avec la clé étrangère
 CREATE TABLE IF NOT EXISTS maintenance (
-    id_maintenance INT AUTO_INCREMENT PRIMARY KEY,
+    id_maintenance INT AUTO_INCREMENT PRIMARY KEY, 
     date_maintenance DATE NOT NULL,
-    id_user INT,
-    FOREIGN KEY (id_user) REFERENCES users(id_user)
+    id_user INT, -- on crée la colonne correspondant à notre clé étrangère qui permettra de lier cette table à la table maintenance
+    FOREIGN KEY (id_user) REFERENCES users(id_user) -- on associe la clé primaire distante en tant que clé entrangère de la table
 );
 
--- Création de la table hardware avec les clés étrangères
+-- création de la table hardware reliée aux tables maitenance et users avec les clés étrangères
 CREATE TABLE IF NOT EXISTS hardware (
     id_pc INT AUTO_INCREMENT PRIMARY KEY,
     marque VARCHAR(255) NOT NULL,
     os VARCHAR(255) NOT NULL,
-    date_achat DATE NOT NULL,
+    date_achat DATE NOT NULL, -- on réutilise les mêmes commandes que pour les tables précedentes, en modifiant les points nécessaires
     id_user INT,
     id_maintenance INT,
     FOREIGN KEY (id_user) REFERENCES users(id_user),
     FOREIGN KEY (id_maintenance) REFERENCES maintenance(id_maintenance)
 );
+
+-- ensuite, on crée un deuxième fichier sql qui permettra d'insérer des données dans la bdd, grâce à des fichiers csv
--- insert_data.sql
+++ insert_data.sql
@@ -1,21 +1,23 @@
--- Utilisation de la base de données existante
+-- utilisation de la base de données précédemment créée
 USE sae51_bdd;
 
--- Insertion des données dans la table "users"
+-- insertion des données dans la table "users"
 LOAD DATA LOCAL INFILE '~/users.csv'
-INTO TABLE users
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+INTO TABLE users -- on va récupérer le contenu du fichier csv associé à chaque table dans les colonnes correpsondantes
+FIELDS TERMINATED BY ',' -- on précise le séparateur permettant de différencier les colonnes dans le csv
+LINES TERMINATED BY '\n'-- on précise comment chaque lignes du csv est délimitée
+IGNORE 1 ROWS; -- on rajoute cette ligne car la première ligne de chaque csv contient l'en-tête de chaque colonne, afin de faciliter la lecture
 
--- Insertion des données dans la table "maintenance"
+-- ensuite, on répète les mêmes commandes pour chaque table
+
+-- insertion des données dans la table "maintenance"
 LOAD DATA LOCAL INFILE '~/maintenance.csv'
 INTO TABLE maintenance
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
--- Insertion des données dans la table "hardware"
+-- insertion des données dans la table "hardware"
 LOAD DATA LOCAL INFILE '~/hardware.csv'
 INTO TABLE hardware
 FIELDS TERMINATED BY ','
